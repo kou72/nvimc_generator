482baf209ee198baaf04dad6ce1c01f2aa897b21
kou72 <genpokopokopoko@gmail.com>
2020-04-12 17:55:49 +0900

    fix checkbox event

--- pages/index.vue
+++ pages/index.vue
@@ -6,16 +6,17 @@
         <v-divider class="mt-8"></v-divider>
       </v-col>
       <v-col cols="6">
-        <v-treeview v-model="selection" :items="this.$store.state.checklist.items" selectable return-object></v-treeview>
+        <v-treeview
+          v-model="selection"
+          :items="this.$store.state.checklist.items"
+          selectable
+          return-object
+          @input="updateText"
+        ></v-treeview>
       </v-col>
       <v-col cols="6">
         <v-btn class="mb-4" @click="downloadText">ダウンロード</v-btn>
         <v-btn class="mb-4" @click="updateText">アップデートテキスト</v-btn>
-        <v-card class="pa-md-4" min-height="50px">
-          <div v-for="node in this.$store.state.checklist.selection" :key="node.id">
-            {{ node.name }}
-          </div>
-        </v-card>
         <v-card class="pa-md-4" min-height="50px">{{ text }}</v-card>
       </v-col>
     </v-row>
