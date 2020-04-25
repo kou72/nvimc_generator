fd79c7c22e0e09148167776f8314a82cb0ecef5f
kou72 <genpokopokopoko@gmail.com>
2020-04-25 21:06:49 +0900

    fix

--- pages/index.vue
+++ pages/index.vue
@@ -1,162 +1,158 @@
 <template>
-  <v-container>
-    <div class="page-container">
-      <!-- Title -->
-      <section class="area">
-        <div class="title">
-          <h1>
-            NeoVimC Generator
-          </h1>
-          <p class="discription">step1 : checked</p>
-          <p class="discription">step2 : download</p>
-          <p class="discription">step3 : open neovim</p>
-        </div>
-      </section>
-      <!-- Contents -->
-      <section class="area">
-        <v-row>
-          <v-col cols="6" class="checkbox">
-            <!-- Auto Install -->
-            <OpenCard title="Default Setting" :open="false">
-              <OpenCard title="Auto Install" :open="true">
-                <v-checkbox
-                  v-for="item in items.install"
-                  :key="item.id"
-                  v-model="selection"
-                  :label="item.name"
-                  :value="item"
-                  hide-details
-                  dense
-                  class="my-0"
-                  @change="updateConfig"
-                />
-              </OpenCard>
-              <!-- Base Setting -->
-              <OpenCard title="Base Setting" :open="true">
-                <v-checkbox
-                  v-for="item in items.base"
-                  :key="item.id"
-                  v-model="selection"
-                  :label="item.name"
-                  :value="item"
-                  hide-details
-                  dense
-                  class="my-0"
-                  @change="updateConfig"
-                />
-              </OpenCard>
-              <!-- Plugin -->
-              <OpenCard title="Plugin" :open="true">
-                <v-checkbox
-                  v-for="item in items.plug"
-                  :key="item.id"
-                  v-model="selection"
-                  :label="item.name"
-                  :value="item"
-                  hide-details
-                  dense
-                  class="my-0"
-                  @change="updateConfig"
-                />
-              </OpenCard>
-              <!-- Design -->
-              <OpenCard title="Visual" :open="true">
-                <v-checkbox
-                  v-for="item in items.visual"
-                  :key="item.id"
-                  v-model="selection"
-                  :label="item.name"
-                  :value="item"
-                  hide-details
-                  dense
-                  class="my-0"
-                  @change="updateConfig"
-                />
-              </OpenCard>
+  <div class="page-container">
+    <!-- Title -->
+    <section class="area">
+      <div class="title">
+        <h1>
+          NeoVimC Generator
+        </h1>
+        <p class="discription">step1 : checked</p>
+        <p class="discription">step2 : download</p>
+        <p class="discription">step3 : open neovim</p>
+      </div>
+    </section>
+    <!-- Contents -->
+    <section class="area">
+      <v-row>
+        <v-col cols="6" class="checkbox">
+          <!-- Auto Install -->
+          <OpenCard title="Default Setting" :open="false">
+            <OpenCard title="Auto Install" :open="true">
+              <v-checkbox
+                v-for="item in items.install"
+                :key="item.id"
+                v-model="selection"
+                :label="item.name"
+                :value="item"
+                hide-details
+                dense
+                class="my-0"
+                @change="updateConfig"
+              />
             </OpenCard>
-            <!-- service  -->
-            <OpenCard title="Serport Service" :open="true">
-              <p class="caption ma-0">*node.js is required!</p>
-              <v-row>
-                <v-col>
-                  <div v-for="(item, index) in items.service" :key="item.id">
-                    <v-checkbox
-                      v-if="index % 2 == 0"
-                      v-model="selection"
-                      :label="item.name"
-                      :value="item"
-                      hide-details
-                      dense
-                      class="my-0"
-                      @change="updateConfig"
-                    />
-                  </div>
-                </v-col>
-                <v-col>
-                  <div v-for="(item, index) in items.service" :key="item.id">
-                    <v-checkbox
-                      v-if="index % 2 == 1"
-                      v-model="selection"
-                      :label="item.name"
-                      :value="item"
-                      hide-details
-                      dense
-                      class="my-0"
-                      @change="updateConfig"
-                    />
-                  </div>
-                </v-col>
-              </v-row>
+            <!-- Base Setting -->
+            <OpenCard title="Base Setting" :open="true">
+              <v-checkbox
+                v-for="item in items.base"
+                :key="item.id"
+                v-model="selection"
+                :label="item.name"
+                :value="item"
+                hide-details
+                dense
+                class="my-0"
+                @change="updateConfig"
+              />
             </OpenCard>
-            <OpenCard title="Mapping" :open="false">
-              <!-- mapping -->
-              <v-textarea class="body-2" :value="map" rows="12" filled hide-details @input="updateMap"></v-textarea>
+            <!-- Plugin -->
+            <OpenCard title="Plugin" :open="true">
+              <v-checkbox
+                v-for="item in items.plug"
+                :key="item.id"
+                v-model="selection"
+                :label="item.name"
+                :value="item"
+                hide-details
+                dense
+                class="my-0"
+                @change="updateConfig"
+              />
             </OpenCard>
-          </v-col>
-          <v-col cols="6" class="config">
-            <!-- config テキスト -->
-            <prism language="js">{{ config }}</prism>
-          </v-col>
-        </v-row>
-      </section>
-      <section class="area">
-        <v-row>
-          <v-col cols="3" class="download">
-            <!-- ダウンロードボタン -->
-            <div style="margin-top: 30vh" />
-            <v-btn class="mb-4" dark outlined depressed width="20vh" height="20vh" fab @click="downloadConfig">
-              <v-icon size="4vw">mdi-download</v-icon>
-            </v-btn>
-            <p class="discription">download</p>
-          </v-col>
-          <!-- ファイルコピー -->
-          <v-col cols="6" class="download">
-            <div style="margin-top: 35vh" />
-            <prism>> mv ~/Downloads/init.vim.txt ~/.config/nvim/init.vim</prism>
-            <div style="margin-top: 9vh" />
-            <p class="discription">move file</p>
-          </v-col>
-          <!-- ファイルを開く -->
-          <v-col cols="3" class="download">
-            <div style="margin-top: 35vh" />
-            <prism>> nvim</prism>
-            <div style="margin-top: 9vh" />
-            <p class="discription">open file</p>
-          </v-col>
-        </v-row>
-      </section>
-    </div>
-  </v-container>
+            <!-- Design -->
+            <OpenCard title="Visual" :open="true">
+              <v-checkbox
+                v-for="item in items.visual"
+                :key="item.id"
+                v-model="selection"
+                :label="item.name"
+                :value="item"
+                hide-details
+                dense
+                class="my-0"
+                @change="updateConfig"
+              />
+            </OpenCard>
+          </OpenCard>
+          <!-- service  -->
+          <OpenCard title="Serport Service" :open="true">
+            <p class="caption ma-0">*node.js is required!</p>
+            <v-row>
+              <v-col>
+                <div v-for="(item, index) in items.service" :key="item.id">
+                  <v-checkbox
+                    v-if="index % 2 == 0"
+                    v-model="selection"
+                    :label="item.name"
+                    :value="item"
+                    hide-details
+                    dense
+                    class="my-0"
+                    @change="updateConfig"
+                  />
+                </div>
+              </v-col>
+              <v-col>
+                <div v-for="(item, index) in items.service" :key="item.id">
+                  <v-checkbox
+                    v-if="index % 2 == 1"
+                    v-model="selection"
+                    :label="item.name"
+                    :value="item"
+                    hide-details
+                    dense
+                    class="my-0"
+                    @change="updateConfig"
+                  />
+                </div>
+              </v-col>
+            </v-row>
+          </OpenCard>
+          <OpenCard title="Mapping" :open="false">
+            <!-- mapping -->
+            <v-textarea class="body-2" :value="map" rows="12" filled hide-details @input="updateMap"></v-textarea>
+          </OpenCard>
+        </v-col>
+        <v-col cols="6" class="config">
+          <!-- config テキスト -->
+          <prism language="js">{{ config }}</prism>
+        </v-col>
+      </v-row>
+    </section>
+    <section class="area">
+      <v-row>
+        <v-col cols="3" class="download">
+          <!-- ダウンロードボタン -->
+          <div style="margin-top: 30vh" />
+          <v-btn class="mb-4" dark outlined depressed width="20vh" height="20vh" fab @click="downloadConfig">
+            <v-icon size="4vw">mdi-download</v-icon>
+          </v-btn>
+          <p class="discription">download</p>
+        </v-col>
+        <!-- ファイルコピー -->
+        <v-col cols="6" class="download">
+          <div style="margin-top: 35vh" />
+          <prism>> mv ~/Downloads/init.vim.txt ~/.config/nvim/init.vim</prism>
+          <div style="margin-top: 9vh" />
+          <p class="discription">move file</p>
+        </v-col>
+        <!-- ファイルを開く -->
+        <v-col cols="3" class="download">
+          <div style="margin-top: 35vh" />
+          <prism>> nvim</prism>
+          <div style="margin-top: 9vh" />
+          <p class="discription">open file</p>
+        </v-col>
+      </v-row>
+    </section>
+  </div>
 </template>
 
 <script>
 import { mapState } from 'vuex'
-// import Passing from '~/components/Passing' // 隠しボックスのアニメーション
 import OpenCard from '~/components/OpenCard'
 
 export default {
   components: {
-    // Passing,
     OpenCard
   },
   data: () => ({
